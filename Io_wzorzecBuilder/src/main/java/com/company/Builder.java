08cffb7a4f76f9da641e58104519e754718ffa5b
mjaworski11 <113938088+mjaworski11@users.noreply.github.com>
2022-09-19 20:52:56 +0200

    Delete Io_wzorzecBuilder directory

--- Io_wzorzecBuilder/src/main/java/com/company/Builder.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public interface Builder {
-    public void dodajkolumne(String[] kolumna);
-    public void dodajtabele(String tabela);
-    public void dodajWartosci(String[] wartosci);
-    public String getSql();
-}
